f9f083baa3c5ff81bab5d37ccea86b0310e9325e
silence <m18679658563@163.com>
2019-09-24 08:50:55 +0800

    add server

--- system/src/main/java/com/silence/system/SystemApplication.java
+++ system/src/main/java/com/silence/system/SystemApplication.java
@@ -2,8 +2,10 @@ package com.silence.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @SpringBootApplication
 public class SystemApplication {
