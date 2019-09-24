c98702ec2e77c7f06c40f917aa04b940c5ee673c
silence <m18679658563@163.com>
2019-09-24 09:17:18 +0800

    add hystrix

---
+++ system/src/main/java/com/silence/system/feign/SystemFeign.java
@@ -0,0 +1,11 @@
+package com.silence.system.feign;
+
+/***
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: silence
+ * Date: 2019-09-24
+ * Time: 上午8:56
+ */
+public class SystemFeign {
+}
---
+++ system/src/main/java/com/silence/system/service/TestService.java
@@ -0,0 +1,11 @@
+package com.silence.system.service;
+
+/***
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: silence
+ * Date: 2019-09-24
+ * Time: 上午8:57
+ */
+public class TestService {
+}
